d8e3bb35354e184d41d5f264a6893135bcabe278
LUNRA <nicolaslarra96@gmail.com>
2016-10-15 17:14:04 -0500

    updated grammar

---
+++ interpreter/src/classes/Interpreter.java
@@ -0,0 +1,32 @@
+package classes;
+
+import java.io.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Interpreter {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		String buffer = new String("");
+		String currLine = new String("");
+		while((currLine = br.readLine()) != null){
+            buffer += currLine.toLowerCase();
+            buffer += "\n";
+		}
+		
+		ANTLRInputStream input = new ANTLRInputStream(buffer);
+		System.out.println(input.toString());
+		MyLanguageLexer lexer = new MyLanguageLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		MyLanguageParser parser = new MyLanguageParser(tokens);
+		ParseTree tree = parser.programa();
+		System.out.println(tree.toStringTree(parser));
+		MyVisitor<Object> loader = new MyVisitor<Object>();
+		loader.visit(tree);
+		br.close();
+
+	}
+
+}
---
+++ interpreter/src/classes/MyVisitor.java
@@ -0,0 +1,5 @@
+package classes;
+
+public class MyVisitor<T> extends MyLanguageBaseVisitor<T>{
+	
+}
